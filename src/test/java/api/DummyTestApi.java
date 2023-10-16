a379626fb0d8aec6b796b6a151aeaf7bb217491b
anatoliipiddubnyi <poddubny.anatoly@gmail.com>
2023-10-16 21:53:05 +0300

    Add Log4J dependency and  configuration

--- src/test/java/api/DummyTestApi.java
+++ src/test/java/api/DummyTestApi.java
@@ -4,10 +4,13 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class DummyTestApi {
+    private static final Logger logger = LoggerFactory.getLogger(DummyTestApi.class);
+
 
     public static void main(String[] args) {
-        Logger logger = LoggerFactory.getLogger(DummyTestApi.class);
-        logger.info("Hello World Log");
+        logger.debug("Debug log message");
+        logger.info("Info log message");
+        logger.error("Error log message");
         System.out.println("This is Demo class example. " +
                 "Tests need to be placed in this package");
 
