7ce2e6d981bf62c0cc5d24350c8d71c783863e89
anatoliipiddubnyi <poddubny.anatoly@gmail.com>
2023-10-28 11:59:49 +0300

    Adding rest assured dependency
    Adding rest assured dependency
    and simple usage example

--- src/main/java/api/DummyApiClient.java
+++ src/main/java/api/DummyApiClient.java
@@ -1,4 +1,19 @@
 package api;
 
+import io.restassured.RestAssured;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 public class DummyApiClient {
+
+    private static final Logger logger = LoggerFactory.getLogger(DummyApiClient.class);
+
+    public static void main(String[] args) {
+        String url = "https://httpbin.org/get";
+        logger.debug("{} {}", "GET", url);
+        String print = RestAssured.given()
+                .get("https://httpbin.org/get")
+                .body().prettyPeek().asPrettyString();
+        logger.debug("{}{}{}", "response:", System.lineSeparator(), print);
+    }
 }
