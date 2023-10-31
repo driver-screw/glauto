02ac5f5f097c749a4941218e6eb5fc6d8483c5d4
anatoliipiddubnyi <poddubny.anatoly@gmail.com>
2023-10-31 14:08:24 +0200

    Move BaseApiTest to test folder

--- src/test/java/api/BaseApiTest.java
+++ src/test/java/api/BaseApiTest.java
@@ -1,15 +1,15 @@
 package api;
 
-import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class BaseApiTest {
     protected final Logger logger = LoggerFactory.getLogger(this.getClass());
-    protected static GitHubApiClient client;
+    protected GitHubApiClient client;
 
-    @BeforeAll
-    public static void setUp() {
+    @BeforeEach
+    public void setUp() {
         client = new GitHubApiClient();
     }
 }
